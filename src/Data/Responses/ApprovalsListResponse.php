92beb89447f641228f8b154791c901cfbac5aaaa
Christian Kaal <christiankaal@MacBook-Pro-van-Christian.local>
2022-02-04 16:21:20 +0100

    Added missing extend

--- src/Data/Responses/ApprovalsListResponse.php
+++ src/Data/Responses/ApprovalsListResponse.php
@@ -2,10 +2,11 @@
 
 namespace CrixuAMG\Simplicate\Data\Responses;
 
+use CrixuAMG\Simplicate\Contracts\Data\SimplicateResponseInterface;
 use CrixuAMG\Simplicate\Data\Hours\Approval;
 use Illuminate\Support\Collection;
 
-class ApprovalsListResponse
+class ApprovalsListResponse extends AbstractDataResponse implements SimplicateResponseInterface
 {
     protected function setData($data)
     {
