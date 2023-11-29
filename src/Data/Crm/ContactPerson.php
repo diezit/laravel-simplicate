bd473915573667f30321d82749ee11866aeaf1d6
Robin van Schuilenburg <robin@diezit.nl>
2023-11-29 11:49:26 +0100

    Implemented ContactPerson

--- src/Data/Crm/ContactPerson.php
+++ src/Data/Crm/ContactPerson.php
@@ -3,18 +3,68 @@
 namespace CrixuAMG\Simplicate\Data\Crm;
 
 use CrixuAMG\Simplicate\Data\AbstractDataObject;
+use CrixuAMG\Simplicate\Data\Project\OrganizationReference;
+use Illuminate\Support\Arr;
 
 class ContactPerson extends AbstractDataObject
 {
-    private array $data;
+    private ?string $id;
+    private ?string $work_function;
+    private ?string $work_email;
+    private ?string $work_mobile;
+    private OrganizationReference $organization;
+    private PersonReference $person;
 
     public function __construct(array $data)
     {
-        $this->data = $data;
+        $this->id = Arr::get($data, 'id');
+        $this->work_function = Arr::get($data, 'work_function');
+        $this->work_email = Arr::get($data, 'work_email');
+        $this->work_mobile = Arr::get($data, 'work_mobile');
+        $this->organization = new OrganizationReference(Arr::get($data, 'organization', []));
+        $this->person = new PersonReference(Arr::get($data, 'person', []));
     }
 
-    public function toArray()
+    public function toArray(): array
     {
-        return (array)$this->data;
+        return [
+            'id' => $this->getId(),
+            'work_function' => $this->getWorkFunction(),
+            'work_email' => $this->getWorkEmail(),
+            'work_mobile' => $this->getWorkMobile(),
+            'organization' => $this->getOrganization()->toArray(),
+            'person' => $this->getPerson()->toArray(),
+        ];
     }
+
+    public function getId(): ?string
+    {
+        return $this->id;
+    }
+
+    public function getWorkFunction(): ?string
+    {
+        return $this->work_function;
+    }
+
+    public function getWorkEmail(): ?string
+    {
+        return $this->work_email;
+    }
+
+    public function getWorkMobile(): ?string
+    {
+        return $this->work_mobile;
+    }
+
+    public function getOrganization(): OrganizationReference
+    {
+        return $this->organization;
+    }
+
+    public function getPerson(): PersonReference
+    {
+        return $this->person;
+    }
+
 }
